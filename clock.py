4958a9cf7c2559e48aa1d940322de13ebcc11e96
Sriramkarthikakula <92425111+Sriramkarthikakula@users.noreply.github.com>
2021-12-25 12:50:23 +0530

    Update clock.py

--- clock.py
+++ clock.py
@@ -3,3 +3,7 @@ from tkinter.ttk import *
 from time import strftime
 root = Tk()
 root.title("clock")
+def time():
+    string = strftime("%d/%B/%Y \n %H:%M:%S %p")
+    label.config(text=string)
+    label.after(1000,time)

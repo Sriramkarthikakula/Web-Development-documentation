4c205c0e89ce08b6a54299e6de772e322c6e52b9
Sriramkarthikakula <92425111+Sriramkarthikakula@users.noreply.github.com>
2021-12-25 12:50:51 +0530

    Update clock.py

--- clock.py
+++ clock.py
@@ -7,3 +7,7 @@ def time():
     string = strftime("%d/%B/%Y \n %H:%M:%S %p")
     label.config(text=string)
     label.after(1000,time)
+label = Label(root, font=("Game Of Squids" , 80) , background="black" , foreground="cyan")
+label.pack(anchor="center")
+time()
+mainloop()
